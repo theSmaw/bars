7b935dc4fbcd58faa6018cf7998e86a942a3b0f7
Smawfield, Benjamin Lukin <benjaminlukin.smawfield@sc.com>
2018-05-24 16:43:49 +0800

    chore(web): Enable sensible caching

--- packages/web/webpack.common.js
+++ packages/web/webpack.common.js
@@ -1,15 +1,42 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    main: './src/index.js',
+    vendor: [
+      'react',
+      'styled-components'
+    ]
+  },
   module: {
     rules: [{
       test: /\.(otf)$/,
       use: ['file-loader']
     }]
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all'
+        }
+      }
+    }
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new webpack.HashedModuleIdsPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Bars'
+    })
+  ],
 };
