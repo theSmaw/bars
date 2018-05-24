7743d1963fd46767858bc2e2ed3947d6c7e51aa6
Smawfield, Benjamin Lukin <benjaminlukin.smawfield@sc.com>
2018-05-24 14:38:57 +0800

    chore: Add fonts

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -1,3 +1,17 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  @font-face {
+    font-family: 'QuickSand-Regular';
+    src: url('../../../assets/fonts/QuickSand-Regular.otf');
+  }
+
+  body {
+    background: #fbf7f2;
+    font-family: 'QuickSand-Regular';
+  }
+`;
+
 function component() {
   var element = document.createElement('div');
 
